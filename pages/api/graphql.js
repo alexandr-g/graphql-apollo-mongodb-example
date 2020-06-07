ee2834cea7fe8c5a9c3aa3d5c0ec5ee7841274c0
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-06-07 18:57:04 +0200

    Adding MongoDB connection to our Apollo Server

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,18 +1,63 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+import { makeExecutableSchema } from 'graphql-tools'
+import { MongoClient } from 'mongodb'
+
+require('dotenv').config()
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    blog: String
+    stars: Int
+  }
+
   type Query {
-    sayHello: String
+    users: [User]!
   }
 `
 
 const resolvers = {
   Query: {
-    sayHello(parent, args, context) {
-      return 'Hello World!'
+    users(_parent, _args, _context, _info) {
+      return _context.db
+        .collection('users')
+        .findOne()
+        .then((data) => {
+          return data.users
+        })
     },
   },
 }
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+
+let db
+
+const apolloServer = new ApolloServer({
+  schema,
+  context: async () => {
+    if (!db) {
+      try {
+        const dbClient = new MongoClient(process.env.MONGO_DB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+
+        if (!dbClient.isConnected()) await dbClient.connect()
+        db = dbClient.db('next-graphql')
+      } catch (e) {
+        console.log('--->error while connecting with graphql context (db)', e)
+      }
+    }
+
+    return { db }
+  },
+})
 
 export const config = {
   api: {
@@ -20,5 +65,4 @@ export const config = {
   },
 }
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
 export default apolloServer.createHandler({ path: '/api/graphql' })
